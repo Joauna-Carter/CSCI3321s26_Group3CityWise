85fd063762963006898dc16e37e88bc6118ca6ad
JC <scidiva12@gmail.com>
2026-03-19 13:23:23 -0500

    Add basic create account feature (no database yet)

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ var PORT = 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// let express read form data
+app.use(express.urlencoded({ extended: true }));
+
 // static files
 app.use(express.static(path.join(__dirname, "public")));
 
 // fake login state for testing
 var isLoggedIn = false;
-var username = "Traveler123";
+var username = "";
 
 // list of cities
 var cities = [
@@ -31,18 +34,37 @@ var cities = [
 
 // home page route
 app.get("/", function(req, res) {
-
     res.render("home", {
         isLoggedIn: isLoggedIn,
         username: username,
         cities: cities
     });
+});
 
+// show create account page
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+// handle create account form
+app.post("/register", function(req, res) {
+    var newUsername = req.body.username;
+    var newPassword = req.body.password;
+
+    if (newUsername == "" || newPassword == "") {
+        res.send("Username and password cannot be empty. <a href='/register'>Go back</a>");
+        return;
+    }
+
+    // fake account creation
+    username = newUsername;
+    isLoggedIn = true;
+
+    res.redirect("/");
 });
 
 // profile page
 app.get("/profile", function(req, res) {
-
     if (!isLoggedIn) {
         res.redirect("/");
         return;
@@ -53,41 +75,25 @@ app.get("/profile", function(req, res) {
         "<p>This is a placeholder profile page.</p>" +
         "<a href='/'>Back to Home</a>"
     );
-
 });
 
-// create account page
-app.get("/register", function(req, res) {
-
-    res.send(
-        "<h1>Create Account Page</h1>" +
-        "<p>This is a placeholder page.</p>" +
-        "<a href='/'>Back to Home</a>"
-    );
-
-});
-
-// login page
+// login page placeholder
 app.get("/login", function(req, res) {
-
     res.send(
         "<h1>Log In Page</h1>" +
-        "<p>This is a placeholder page.</p>" +
+        "<p>This is a placeholder page for now.</p>" +
         "<a href='/'>Back to Home</a>"
     );
-
 });
 
 // logout route
 app.get("/logout", function(req, res) {
-
+    isLoggedIn = false;
+    username = "";
     res.redirect("/");
-
 });
 
 // start server
 app.listen(PORT, function() {
-
     console.log("Server running at http://localhost:" + PORT);
-
 });
